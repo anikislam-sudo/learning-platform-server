49e4fd1c4aab808ea8ad45204a96c82db2faf28f
unknown <islammoshiu@gmail.com>
2022-10-24 23:24:40 +0600

    created data folder and category file added

---
+++ Data/Categories.js
@@ -0,0 +1,26 @@
+[
+    {
+      "id": "01",
+      "name": "HTML"
+    },
+    {
+      "id": "02",
+      "name": "Css"
+    },
+    {
+      "id": "03",
+      "name": "Bootstrap"
+    },
+    {
+      "id": "04",
+      "name": "Tailwind"
+    },
+    {
+      "id": "05",
+      "name": "JavaScript"
+    },
+    {
+      "id": "06",
+      "name": "React"
+    },
+]
